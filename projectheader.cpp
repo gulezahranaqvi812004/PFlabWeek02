6a9210ff2c3a83091009cbeef827a494aa675ad7
gulezahranaqvi <gulezahrasyeda980@gmail.com>
2022-12-16 07:38:13 -0800

    pflab2

---
+++ projectheader.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout << "***********************************" <<endl;
+cout << "**                               **"<<endl;
+cout << "**     university admission      **" <<endl;
+cout << "**     management system         **" <<endl;
+cout << "**                               **" <<endl;
+cout << "***********************************" <<endl;
+cout << "***********************************" <<endl;
+}
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout << "my name is Gul-e-Zahra"<< endl << "I am a girl" << endl;
+
+cout << "my roll no is 8";
+
+
+
+
+}
